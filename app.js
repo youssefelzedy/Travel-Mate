a5055cd3262b6e83c9d6250b7c56740583f13dec
youssef2272002salah <youssef22772002salah@gmail.com>
2024-08-27 03:16:31 +0300

    adding some feature for security like xss ratelimit and helmet for more details

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const dbConnect = require('./dbConnect')
 const dotenv = require('dotenv')
 const path = require('path')
 const cors = require('cors')
+// for secure 
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const mongoSanitize = require('express-mongo-sanitize')
+const rateLimit = require('express-rate-limit')
 
 const cookieParser = require('cookie-parser')
 const errorControllers = require('./controllers/errorControllers')
@@ -22,6 +27,17 @@ dbConnect()
 const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(cors())
+
+app.use(helmet())
+app.use(xss())
+app.use(mongoSanitize())
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+})
+app.use('/api', limiter)
+
+app.use(express.urlencoded({ extended: true }))
 
 app.use(express.json())
 app.use(cookieParser())
