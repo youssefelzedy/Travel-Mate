70fab927d1320f6dd7e10ddbdd196f7b62d3abdb
Youssef Elzedy <youssefelzedy90@gmail.com>
2025-03-20 02:52:11 +0200

    refactor: Remove placeControllers and streamline userControllers error handling

--- Back-End/controllers/errorControllers.js
+++ Back-End/controllers/errorControllers.js
@@ -1,45 +1,60 @@
 const AppError = require(`${__dirname}/../utils/appError`);
-const handleCastErrorDB = err => {
+const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}.`;
   return new AppError(message, 400);
 };
 
-const handleDuplicateFieldsDB = err => {
-   return new AppError({ english: 'Duplicate field value Please use another email!', arabic: 'قيمة الحقل المكررة:يرجى استخدام قيمة مختلفة!'}, 400);
+const handleDuplicateFieldsDB = (err) => {
+  return new AppError(
+    {
+      english: "Duplicate field value Please use another email!",
+      arabic: "قيمة الحقل المكررة:يرجى استخدام قيمة مختلفة!",
+    },
+    400
+  );
 };
 
-
-
 const handleJWTError = () =>
-  new AppError({ english: 'Invalid token. Please log in again!', arabic: 'رمز غير صالح. يرجى تسجيل الدخول مرة اخرى!'}, 401);
+  new AppError(
+    {
+      english: "Invalid token. Please log in again!",
+      arabic: "رمز غير صالح. يرجى تسجيل الدخول مرة اخرى!",
+    },
+    401
+  );
 
 const handleJWTExpiredError = () =>
-  new AppError({ english: 'Your token has expired! Please log in again.', arabic: 'انتهت صلاحية رمزك. يرجى تسجيل الدخول مرة اخرى!' }, 401);
+  new AppError(
+    {
+      english: "Your token has expired! Please log in again.",
+      arabic: "انتهت صلاحية رمزك. يرجى تسجيل الدخول مرة اخرى!",
+    },
+    401
+  );
 
-const handleValidationErrorDB = err => {
-  const errors = Object.values(err.errors).map(el => el.message);
-  const message = `Invalid input data. ${errors.join('. ')}`;
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join(". ")}`;
   return new AppError(message, 400);
 };
 
-
 const sendErrorProd = (err, req, res) => {
   // A) API
-  if (req.originalUrl.startsWith('/api')) {
+  if (req.originalUrl.startsWith("/api")) {
     // A) Operational, trusted error: send message to client
     if (err.isOperational) {
       return res.status(err.statusCode).json({
         status: err.status,
-        message: err.message
+        message: err.message,
       });
     }
 
     return res.status(500).json({
-      status: 'error',
+      status: "error",
       message: {
-        english: 'Something went wrong!',
-        arabic: 'حدث خطأ ما'
-      }
+        english: "Something went wrong!",
+        arabic: "حدث خطأ ما",
+      },
     });
   }
 
@@ -47,40 +62,39 @@ const sendErrorProd = (err, req, res) => {
   // A) Operational, trusted error: send message to client
   if (err.isOperational) {
     console.log(err);
-    return res.status(err.statusCode).render('error', {
-      title: 'Something went wrong!',
-      msg: err.message
+    return res.status(err.statusCode).render("error", {
+      title: "Something went wrong!",
+      msg: err.message,
     });
   }
   // B) Programming or other unknown error: don't leak error details
   // 1) Log error
-  console.error('ERROR 💥', err);
+  console.error("ERROR 💥", err);
   // 2) Send generic message
-  return res.status(err.statusCode).render('error', {
-    title: 'Something went wrong!',
-    msg: 'Please try again later.'
+  return res.status(err.statusCode).render("error", {
+    title: "Something went wrong!",
+    msg: "Please try again later.",
   });
 };
 
 module.exports = (err, req, res, next) => {
-
   err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-    let error = { ...err };
-    error.message = err.message;
+  err.status = err.status || "error";
+  let error = { ...err };
+  error.message = err.message;
 
-    if (err.name === 'ValidationError') {
-      const errors = Object.values(err.errors).map(e => ({
-          messages: e.message // Assuming the message is an object with english and arabic properties
-      }));
-      console.log(errors[0].messages.english);
-      console.log(errors[0].messages.arabic);
-return res.status(400).json({ messages: errors[0].messages });
-    }
-    if (error.name === 'CastError') error = handleCastErrorDB(error);
-    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    if (error.name === 'JsonWebTokenError') error = handleJWTError();
-    if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
-    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
-    sendErrorProd(error, req, res);
+  if (err.name === "ValidationError") {
+    const errors = Object.values(err.errors).map((e) => ({
+      messages: e.message, // Assuming the message is an object with english and arabic properties
+    }));
+    console.log(errors[0].messages.english);
+    console.log(errors[0].messages.arabic);
+    return res.status(400).json({ messages: errors[0].messages });
+  }
+  if (error.name === "CastError") error = handleCastErrorDB(error);
+  if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+  if (error.name === "JsonWebTokenError") error = handleJWTError();
+  if (error.name === "ValidationError") error = handleValidationErrorDB(error);
+  if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
+  sendErrorProd(error, req, res);
 };
--- Back-End/controllers/journeyControllers.js
+++ Back-End/controllers/journeyControllers.js
@@ -6,166 +6,157 @@ const e = require("express");
 const microbus = require(`${__dirname}/../utils/microbus`);
 const taxi = require(`${__dirname}/../utils/taxi`);
 
-
 exports.getAllJourneys = catchAsync(async (req, res, next) => {
-    const journeys = await Journey.find();
-    res.status(200).json({
-        status: "success",
-        massage: {
-            english: "Journeys retrieved successfully",
-            arabic: "تم تحميل المسارات بنجاح"
-        },
-        results: journeys.length,
-        data: {
-            journeys
-        }
-    });
-})
-
+  const journeys = await Journey.find();
+  res.status(200).json({
+    status: "success",
+    massage: {
+      english: "Journeys retrieved successfully",
+      arabic: "تم تحميل المسارات بنجاح",
+    },
+    results: journeys.length,
+    data: {
+      journeys,
+    },
+  });
+});
 
 exports.getJourney = catchAsync(async (req, res, next) => {
-    const journey = await Journey.findById(req.params.id);
-    res.status(200).json({
-        status: "success",
-        massage: {
-            english: "Journey retrieved successfully",
-            arabic: "تم تحميل المسار بنجاح"
-        },
-        data: {
-            journey
-        }
-    });
-})
-
+  const journey = await Journey.findById(req.params.id);
+  res.status(200).json({
+    status: "success",
+    massage: {
+      english: "Journey retrieved successfully",
+      arabic: "تم تحميل المسار بنجاح",
+    },
+    data: {
+      journey,
+    },
+  });
+});
 
 exports.createJourney = catchAsync(async (req, res, next) => {
-    const journey = await Journey.create({
-        city: req.body.city,
-        transport: req.body.transport,
-        location: req.body.location
-      });
+  const journey = await Journey.create({
+    city: req.body.city,
+    transport: req.body.transport,
+    location: req.body.location,
+  });
 
-    const user = await User.findById(req.user.id);
-    user.journeys.push(journey._id);
-    await user.save();
+  const user = await User.findById(req.user.id);
+  user.journeys.push(journey._id);
+  await user.save();
 
+  res.status(201).json({
+    status: "success",
+    massage: {
+      english: "Journey created successfully",
+      arabic: "تم انشاء المسار بنجاح",
+    },
+    data: {
+      journey,
+    },
+  });
+});
 
-    res.status(201).json({
-        status: "success",
-        massage: {
-            english: "Journey created successfully",
-            arabic: "تم انشاء المسار بنجاح"
-        },
-        data: {
-            journey
-        }
-    });
-})
-
-exports.updateJourney = catchAsync(async (req, res, next) => {  
-    const journey = await Journey.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true
-    }); 
-    res.status(200).json({
-        status: "success",
-        massage: {
-            english: "Journey updated successfully",
-            arabic: "تم تحديث المسار بنجاح"
-        },
-        data: {
-            journey
-        }
-    });
-})
-
+exports.updateJourney = catchAsync(async (req, res, next) => {
+  const journey = await Journey.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: "success",
+    massage: {
+      english: "Journey updated successfully",
+      arabic: "تم تحديث المسار بنجاح",
+    },
+    data: {
+      journey,
+    },
+  });
+});
 
 exports.deleteJourney = catchAsync(async (req, res, next) => {
-    await Journey.findByIdAndDelete(req.params.id);
-    res.status(204).json({
-        status: "success",
-        massage: {
-            english: "Journey deleted successfully",
-            arabic: "تم حذف المسار بنجاح"
-        },
-        data: null
-    });
-})
+  await Journey.findByIdAndDelete(req.params.id);
+  res.status(204).json({
+    status: "success",
+    massage: {
+      english: "Journey deleted successfully",
+      arabic: "تم حذف المسار بنجاح",
+    },
+    data: null,
+  });
+});
 
 exports.searchMicrobus = catchAsync(async (req, res, next) => {
-    try {
-        const location_lat = req.body.location.lat;
-        const location_lng = req.body.location.lng;
-        const destination_lat = req.body.destination.lat;
-        const destination_lng = req.body.destination.lng;
-    
-        const location = { lat: location_lat, lng: location_lng };
-        const destination = { lat: destination_lat, lng: destination_lng };
+  try {
+    const location_lat = req.body.location.lat;
+    const location_lng = req.body.location.lng;
+    const destination_lat = req.body.destination.lat;
+    const destination_lng = req.body.destination.lng;
 
-    
-        const coreMicrobus = new microbus(location, destination);
-        coreMicrobus.initializeData().then(() => {
-            res.status(200).json({
-                status: "success",
-                massage: {
-                    english: "Microbus search completed successfully",
-                    arabic: "تم البحث عن الميكروباص بنجاح"
-                },
-                data: {
-                    result: coreMicrobus.finalResult
-                }
-            });
-        });
-    }
-    catch (err) {
-        res.status(400).json({
-            status: "fail",
-            massage: {
-                english: "Microbus search failed",
-                arabic: "فشل البحث عن الميكروباص"
-            },
-            data: {
-                err
-            }
-        });
-    }
-})
+    const location = { lat: location_lat, lng: location_lng };
+    const destination = { lat: destination_lat, lng: destination_lng };
+
+    const coreMicrobus = new microbus(location, destination);
+    coreMicrobus.initializeData().then(() => {
+      res.status(200).json({
+        status: "success",
+        massage: {
+          english: "Microbus search completed successfully",
+          arabic: "تم البحث عن الميكروباص بنجاح",
+        },
+        data: {
+          result: coreMicrobus.finalResult,
+        },
+      });
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      massage: {
+        english: "Microbus search failed",
+        arabic: "فشل البحث عن الميكروباص",
+      },
+      data: {
+        err,
+      },
+    });
+  }
+});
 
 exports.searchTaxi = catchAsync(async (req, res, next) => {
-    try {
-        const location_lat = req.body.location.lat;
-        const location_lng = req.body.location.lng;
-        const destination_lat = req.body.destination.lat;
-        const destination_lng = req.body.destination.lng;
-    
-        const location = { lat: location_lat, lng: location_lng };
-        const destination = { lat: destination_lat, lng: destination_lng };
+  try {
+    const location_lat = req.body.location.lat;
+    const location_lng = req.body.location.lng;
+    const destination_lat = req.body.destination.lat;
+    const destination_lng = req.body.destination.lng;
 
-    
-        const coreTaxi = new taxi(location, destination);
-        coreTaxi.initialize().then(() => {
-            res.status(200).json({
-                status: "success",
-                massage: {
-                    english: "Taxi search completed successfully",
-                    arabic: "تم البحث عن تاكسي بنجاح"
-                },
-                data: {
-                    result: coreTaxi.finalResult
-                }
-            });
-        });
-    }
-    catch (err) {
-        res.status(400).json({
-            status: "fail",
-            massage: {
-                english: "Taxi search failed",
-                arabic: "فشل البحث عن تاكسي"
-            },
-            data: {
-                err
-            }
-        });
-    }
-})
+    const location = { lat: location_lat, lng: location_lng };
+    const destination = { lat: destination_lat, lng: destination_lng };
+
+    const coreTaxi = new taxi(location, destination);
+    coreTaxi.initialize().then(() => {
+      res.status(200).json({
+        status: "success",
+        massage: {
+          english: "Taxi search completed successfully",
+          arabic: "تم البحث عن تاكسي بنجاح",
+        },
+        data: {
+          result: coreTaxi.finalResult,
+        },
+      });
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      massage: {
+        english: "Taxi search failed",
+        arabic: "فشل البحث عن تاكسي",
+      },
+      data: {
+        err,
+      },
+    });
+  }
+});
