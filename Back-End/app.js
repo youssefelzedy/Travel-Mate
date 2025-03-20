421cdc9922b01e6911e0b8292bd25dbd662f3be3
Youssef Elzedy <youssefelzedy90@gmail.com>
2025-03-20 02:52:19 +0200

    style: Standardize code formatting and improve readability in app.js

--- Back-End/app.js
+++ Back-End/app.js
@@ -1,51 +1,50 @@
-const express = require('express')
-const dbConnect = require('./dbConnect')
-const dotenv = require('dotenv')
-const path = require('path')
-const cors = require('cors')
-// for secure 
-const helmet = require('helmet')
-const xss = require('xss-clean')
-const mongoSanitize = require('express-mongo-sanitize')
-const rateLimit = require('express-rate-limit')
+const express = require("express");
+const dbConnect = require("./dbConnect");
+const dotenv = require("dotenv");
+const path = require("path");
+const cors = require("cors");
+// for secure
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const mongoSanitize = require("express-mongo-sanitize");
+const rateLimit = require("express-rate-limit");
 
-const cookieParser = require('cookie-parser')
-const errorControllers = require(`${__dirname}/controllers/errorControllers`)
+const cookieParser = require("cookie-parser");
+const errorControllers = require(`${__dirname}/controllers/errorControllers`);
 
-dotenv.config()
+dotenv.config();
 
-if (process.env.NODE_ENV === 'development') {
-  process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'
+if (process.env.NODE_ENV === "development") {
+  process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
 }
 
-const userRoutes = require('./routes/userRoutes')
-const placeRoutes = require('./routes/placeRoutes')
-const journeyRoutes = require(`${__dirname}/routes/journeyRoutes`)
+const userRoutes = require("./routes/userRoutes");
+const placeRoutes = require("./routes/placeRoutes");
+const journeyRoutes = require(`${__dirname}/routes/journeyRoutes`);
 
-dbConnect()
-const app = express()
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(cors())
+dbConnect();
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
-app.use(helmet())
-app.use(xss())
-app.use(mongoSanitize())
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100
-})
-app.use('/api', limiter)
+  max: 100,
+});
+app.use("/api", limiter);
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
-// api routes
-app.use('/api/v1/users', userRoutes)
-app.use('/api/v1/places', placeRoutes)
-app.use('/api/v1/journeys', journeyRoutes)
+// API Routes
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/places", placeRoutes);
+app.use("/api/v1/journeys", journeyRoutes);
 
-
-app.use(errorControllers)
-module.exports = app
+app.use(errorControllers);
+module.exports = app;
