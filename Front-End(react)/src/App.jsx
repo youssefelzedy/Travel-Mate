301c1771bd39d6985da24f07785d43f854cc3556
SalahMSwefy <salahm.swefy@gmali.com>
2025-04-16 14:06:34 +0200

    add some changes

--- Front-End(react)/src/App.jsx
+++ Front-End(react)/src/App.jsx
@@ -1,6 +1,6 @@
 import "leaflet/dist/leaflet.css";
 import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
-
+import "./styles/style.css";
 import AppLayout from "./AppLayout";
 
 function App() {
