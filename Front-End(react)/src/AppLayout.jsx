d84d423c60f547efbc93b1abe76b51074680e0ed
SaadSamir7 <saadsamir7177@gmail.com>
2025-04-16 14:29:18 +0200

    sidebar

--- Front-End(react)/src/AppLayout.jsx
+++ Front-End(react)/src/AppLayout.jsx
@@ -1,11 +1,21 @@
 import MyMap from "./Map";
+import Sidebar from "./Sidebar";
 
 function AppLayout() {
-    return (
-        <div>
-            <MyMap />
-        </div>
-    );
+  return (
+    <div className="app-layout">
+      <Sidebar />
+      <MyMap />
+      <style jsx="true">{`
+        .app-layout {
+          position: relative;
+          width: 100%;
+          height: 100vh;
+          overflow: hidden;
+        }
+      `}</style>
+    </div>
+  );
 }
 
 export default AppLayout;
