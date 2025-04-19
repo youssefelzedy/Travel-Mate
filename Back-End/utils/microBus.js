aacd60723ab73397f99d00833500fef6a58be7b8
Youssef Elzedy <youssefelzedy90@gmail.com>
2025-04-19 19:52:33 +0200

    Integrate Dijkstra's algorithm into BusLine for graph processing and update data loading

--- Back-End/utils/microBus.js
+++ Back-End/utils/microBus.js
@@ -1,4 +1,5 @@
 const fs = require("fs").promises;
+const { dijkstra } = require("./distanceCalculation");
 
 class BusLine {
   pointsLine = [];
@@ -20,7 +21,7 @@ class BusLine {
     for (let dx = -range; dx <= range; dx++) {
       for (let dy = -range; dy <= range; dy++) {
         const neighborKey = `${tileX + dx},${tileY + dy}`;
-        console.log("neighborKey", neighborKey);
+        //   console.log("neighborKey", neighborKey);
         if (tilePoints[neighborKey]) {
           neighbors.push(...tilePoints[neighborKey]);
         }
@@ -39,8 +40,21 @@ class BusLine {
     return neighbors;
   }
 
-  _processGraph() {
-    // This method is intended to process the graph data for the bus line.
+  // This method is intended to process the graph data for the bus line.
+  _processGraph(pointsRelation) {
+    let graph = {};
+
+    for (const pointLocation of this.neighborPointsLocation) {
+      for (const pointDestination of this.neighborPointsDestination) {
+        const startNode = pointLocation.name;
+        const endNode = pointDestination.name;
+        const result = dijkstra(pointsRelation, startNode, endNode);
+        console.log("pointLocation", pointLocation);
+        console.log("pointDestination", pointDestination);
+        console.log(result);
+        console.log("==========================");
+      }
+    }
   }
   _preparingResult() {
     // This method is intended to prepare the final result after processing the graph.
@@ -51,16 +65,18 @@ class BusLine {
     const fileContent = await fs.readFile(fileName, "utf8");
     const data = JSON.parse(fileContent);
 
-    return data; 
+    return data;
   }
 
   projectLatLng(lat, lng, tileSize = 256, zoom = 19) {
     const scale = tileSize * Math.pow(2, zoom); // e.g. 256, 512, 1024, etc.
 
-    const x = (lng + 180) / 360 * scale;
+    const x = ((lng + 180) / 360) * scale;
 
     const latRad = (lat * Math.PI) / 180;
-    const y = (1 - Math.log(Math.tan(latRad) + 1 / Math.cos(latRad)) / Math.PI) / 2 * scale;
+    const y =
+      ((1 - Math.log(Math.tan(latRad) + 1 / Math.cos(latRad)) / Math.PI) / 2) *
+      scale;
 
     return { x, y };
   }
@@ -85,7 +101,7 @@ class BusLine {
       tileYLocation
     );
 
-    console.log("neighborPointsLocation", this.neighborPointsLocation);
+    //  console.log("neighborPointsLocation", this.neighborPointsLocation);
     if (this.neighborPointsLocation.length === 0) {
       throw new Error("Location not found");
     }
@@ -104,20 +120,19 @@ class BusLine {
       tileXDestination,
       tileYDestination
     );
-    console.log(
-      "neighborPointsDestination",
-      this.neighborPointsDestination
-    );
+    //  console.log("neighborPointsDestination", this.neighborPointsDestination);
     if (this.neighborPointsDestination.length === 0) {
       throw new Error("Destination not found");
     }
 
     // Process the graph
-    this._processGraph();
+    const pointsRelation = await this.loadData(
+      "storage/relations_with_dis-and-fee.json"
+    );
+    //  console.log("pointsRelation", pointsRelation);
+    this._processGraph(pointsRelation);
 
     // Prepare the result
     this._preparingResult();
   }
 }
-
-module.exports = BusLine;
